17a9aecf7803f35ccb373e6928495f1d43359f82
dinobet995 <dinorahbettman9956199@gmail.com>
2024-07-27 15:15:19 +0100

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", // URL
+      accounts: ["0x"], // pk
+    },
+  },
+};
